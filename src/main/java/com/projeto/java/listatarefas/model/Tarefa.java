81a44675e0bbd9ff593980f2f73d26dd71ccfdd5
Rennosmx <rennomx@gmail.com>
2024-01-10 21:00:22 -0300

    Primeiro commit

---
+++ src/main/java/com/projeto/java/listatarefas/model/Tarefa.java
@@ -0,0 +1,12 @@
+package com.projeto.java.listatarefas.model;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class Tarefa {
+
+    private String descricao;
+
+}
---
+++ src/test/java/com/projeto/java/listatarefas/ListatarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.java.listatarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListatarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
